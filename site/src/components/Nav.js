2d50c95683b46dd9882a1e76c8f6f9c4d6e8e0fb
Austin <aborn@mdstaff.com>
2018-03-18 12:39:50 -0700

    initial commit

---
+++ site/src/components/Nav.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { login, logout, isLoggedIn } from '../utils/AuthService';
+import '../App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+const style = {
+  margin: 12,
+};
+class Nav extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/">Home</Link>
+        </div>
+        <ul className="nav navbar-nav">
+          <li> {
+            (isLoggedIn()) ? <Link to="/">Food Jokes</Link> : '' 
+          }
+          </li>
+          <li>
+            {
+              (isLoggedIn()) ? <Link to="/special">Celebrity Jokes</Link> : ''
+            }
+
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            {
+              (isLoggedIn()) ? (<RaisedButton onClick={() => logout()} label="Logout" secondary={true} style={style}></RaisedButton>)
+                : (<RaisedButton onClick={() => login()} label="Login" primary={true} style={style}></RaisedButton>)
+            }
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
---
+++ site/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import Profile from './components/Profile';
+import Callback from './components/Callback';
+import { Router, Route, browserHistory } from 'react-router';
+import { requireAuth } from './utils/AuthService';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+const Root = () => {
+  return (
+    <div className="container">
+      <MuiThemeProvider>
+        <Router history={browserHistory}>
+          <Route path="/" component={Profile} />
+          <Route path="/foodjokes" component={FoodJokes} onEnter={requireAuth} />
+          <Route path="/special" component={CelebrityJokes} onEnter={requireAuth} />
+          <Route path="/callback" component={Callback} />
+        </Router>
+      </MuiThemeProvider>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
